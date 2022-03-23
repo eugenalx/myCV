0fe980a0ab243a6e9554f628bc9fe4e190e433a0
Eugen <eugenalx@gmail.com>
2022-03-23 20:40:32 +0100

    layout complete 1

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const allSelection = document.querySelector('.main-content');
 function pageTransition(){
     for (let i =0; i < sectBtn.length; i++){
         sectBtn[i].addEventListener('click', function(){
-            let currentBtn = document.querySelectorAll('.active-btn');
+            let currentBtn = document.querySelectorAll('active-btn');
             currentBtn[0].className = currentBtn[0].className.replace('active-btn','');
             this.className += ' active-btn';
         })
